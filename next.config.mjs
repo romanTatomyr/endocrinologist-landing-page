26f0f9ad6fd71a52150a1b3a01a0f45863a63c52
Roman Tatomyr <tatomyr.roman@gmail.com>
2026-04-10 01:14:19 +0300

    images dynam

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  output: 'export',
-  //basePath: '/endocrinologist-landing-page',
+  // Якщо ми на GitHub (є GITHUB_ACTIONS), додаємо префікс. Якщо на Vercel — ні.
+  basePath: process.env.GITHUB_ACTIONS ? '/endocrinologist-landing-page' : '',
 }
 
 export default nextConfig
